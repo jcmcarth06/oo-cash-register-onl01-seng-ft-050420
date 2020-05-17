41d386d134e7092d1fcba640f87cd30cd250c5bf
Joshua McArthur <jcmcarth06@gmail.com>
2020-05-17 23:52:25 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,7 @@ class CashRegister
   def add_item(name, amount, quantity=1)
     self.total += amount * quantity
     quantity.times do
-      items << name
+      items.push = name
     end
     self.last_transaction = amount * quantity
   end
